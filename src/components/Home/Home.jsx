4baf458d31b8b33a778a20bfcdd6bcb2d7effd27
mahizaman490 <mahizaman490@gmail.com>
2023-09-14 21:48:31 +0600

    json file and all its data is created correctly

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Home.css'
+const Home = () => {
+    return (
+        <div className='container'>
+            <h1>hellow world</h1>
+        </div>
+    );
+};
+
+export default Home;
